c48c079ff98545cbd36160df1454787a31e3bc74
Antonio <antoniosousac2003@gmail.com>
2023-09-23 01:04:24 -0300

    +1

--- exercise_1.c
+++ exercise_1.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
 
+int main(int argc, char *argv[]){
+
+    printf("Mensagem_1\n");
+    printf("Mensagem_2\n");
+    printf("Mensagem_3\n");
+    printf("Mensagem_4\n");
+    printf("Mensagem_5\n");
+
+}
