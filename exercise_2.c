0ee4a20b7a4337b107d07492a75208704139e07e
Antonio <antoniosousac2003@gmail.com>
2023-09-23 18:49:10 -0300

    +1

---
+++ exercise_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade;
+    float pi;
+
+    idade = 10;
+    pi = 3.141592653589793;
+
+    printf("Antonio é lindo e tem %d anos.\n\n", idade);
+    printf("O número de pi vale %.5f.\n\n", pi);
+
+
+}
