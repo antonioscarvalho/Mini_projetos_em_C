8737adee92c89e19f11457a804f90b3d7ebd37f1
Antonio <antoniosousac2003@gmail.com>
2023-10-07 17:15:14 -0300

    +1

--- exercise_14.c
+++ exercise_14.c
@@ -3,24 +3,24 @@
 int main() {
     int idade, ano;
 
-    printf("Digite o ano de nascimento do atleta:\n");
+    printf("Digite o ano de nascimento que o atleta nasceu:\n");
     scanf("%d", &ano);
 
     idade = 2023 - ano;
 
     if (idade <= 9) {
-        printf("A pessoa se enquadra na categoria mirim.");
+        printf("A pessoa se enquadra na categoria mirim.\n");
     }
-    else if (9 < idade <= 14) {
-        printf("A pessoa se enquadra na categoria infantil.");
+    else if (9 < idade && idade <= 14) {
+        printf("A pessoa se enquadra na categoria infantil.\n");
     }
-    else if (14 < idade <= 19) {
-        printf("A pessoa se enquadra na categoria junior.");
+    else if (14 < idade && idade <= 19) {
+        printf("A pessoa se enquadra na categoria junior.\n");
     }
-    else if (19 < idade <= 20) {
-        printf("A pessoa se enquadra na categoria sênior.");
+    else if (19 < idade && idade <= 20) {
+        printf("A pessoa se enquadra na categoria sênior.\n");
     }
     else {
-        printf("Não pode concorrer.");
+        printf("Não pode concorrer.\n");
     }
 }
